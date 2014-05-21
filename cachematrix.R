a3a7403a7f7a2bb34a6b95569565828863b16430
johndn319 <John.D.Nguyen@comcast.net>
2014-05-21 11:30:45 -0500

    First change to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,46 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## makeCacheMatrix is a function that creates a special "matrix" object that can cache its inverse.
+	makeCacheMatrix <- function(x = matrix()) {
+	## if an object is called without a method then set them to NULL
+	## the operator <<- allows the object can be changed outside its function	
+		m <- NULL
+		## set change the matrix x outsite its function with <<- operators
+		set <- function(y) {
+			x <<- y
+			m <<- NULL
+		}
+		## get returns the matrix x
+		get <- function() x
+		## setinverse creates an inverse matrix m via the solve function and assigns that m back outside its function - note of <<- operator
+		setinverse <- function(solve) m <<- solve(x)
+		## getinverse returns the inverse matrix m
+		getinverse <- function() m
+		## here are the returns
+		list(set = set, get = get,
+			setinverse = setinverse, 
+			getinverse = getinverse)
+	}
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
+	cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+		## calls the getinverse() method for the matrix x
+		m <- x$getinverse()
+		## check if m is not null, meaning it has cache the returns the cache	
+		if(!is.null(m)) {
+			message("getting cached data")
+			return(m)
+		 }
+		## these codes below for the one that does not has cache
+		data <- x$get()
+		m <- solve(data, ...)
+		x$setinverse(m)
+		m
+	}
